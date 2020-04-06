9904ea6c2be06898e0b534ebe8ff182b891df5c7
Julien Capellari <julien@capellari.net>
2020-04-07 00:24:40 +0200

    update: http requests from session

--- lucifer/session.py
+++ lucifer/session.py
@@ -1,5 +1,6 @@
 import aiohttp
 from typing import Optional
+from urllib.parse import urljoin
 
 from lucifer.base.session import BaseSession
 
@@ -17,29 +18,49 @@ class LuciferSession(BaseSession):
         self._id = id
         self._secret = secret
 
-    async def _login(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.post(f'{self.endpoint}/daemons/login', json={'id': self._id, 'secret': self._secret}) as resp:
-                data = await resp.json()
-
-                self._token = data['token']
-                self._daemon = data['daemon']
+    def _url(self, path: str):
+        return urljoin(self.endpoint, path)
 
     async def open(self):
         # Already opened
         if self._session is not None:
             return
 
-        # Connected ?
-        if self._token is None:
-            await self._login()
-
         # Open session
-        headers = {
-            'Authorization': f'Bearer {self._token}'
-        }
+        self._session = aiohttp.ClientSession()
 
-        self._session = aiohttp.ClientSession(headers=headers)
+        # Connect
+        if self._token is None:
+            await self.login()
+
+    async def login(self):
+        data = await self.post('/daemons/login', data={'id': self._id, 'secret': self._secret})
+
+        self._token = data['token']
+        self._daemon = data['daemon']
+
+    async def request(self, method: str, path: str, *_, **kwargs):
+        try:
+            async with self._session.request(method, self._url(path), **kwargs) as resp:
+                return await resp.json()
+
+        except aiohttp.ClientResponseError as err:
+            if err.status == 401:
+                self._token = ''
+
+            raise
+
+    async def get(self, path: str, *, params: dict = None):
+        return await self.request('get', path, params=params, headers=self.headers)
+
+    async def post(self, path: str, *, data: dict, params: dict = None):
+        return await self.request('post', path, params=params, headers=self.headers, json=data)
+
+    async def put(self, path: str, *, data: dict, params: dict = None):
+        return await self.request('put', path, params=params, headers=self.headers, json=data)
+
+    async def delete(self, path: str, *, params: dict = None):
+        return await self.request('delete', path, params=params, headers=self.headers)
 
     async def close(self):
         if self._session is None:
@@ -52,4 +73,10 @@ class LuciferSession(BaseSession):
     # Properties
     @property
     def endpoint(self):
-        return 'http://localhost:4200/api'
+        return 'http://localhost:8000/api'
+
+    @property
+    def headers(self):
+        return {
+            'Authorization': f'Bearer {self._token}' if self._token else None
+        }
