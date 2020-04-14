f0e308ce8269ade77a057e38cd5b3085c35fbc8d
Julien Capellari <julien.capellari@neoxia.com>
2020-04-14 10:26:53 +0200

    update: update routes

--- lucifer/daemons/client.py
+++ lucifer/daemons/client.py
@@ -1,13 +1,16 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from lucifer.bases.client import BaseClient
-from lucifer.daemons.daemon import SimpleDaemon, Daemon
+from lucifer.users.user import User
+
+from .daemon import SimpleDaemon, Daemon
 
 
+# Client
 class DaemonsClient(BaseClient):
     # Methods
     async def list(self) -> List[SimpleDaemon]:
-        res = await self._session.get(f'daemons')
+        res = await self._session.get('daemons')
         return [SimpleDaemon(r) for r in res]
 
     async def create(self, name: Optional[str] = None) -> Daemon:
@@ -16,10 +19,21 @@ class DaemonsClient(BaseClient):
         if name is not None:
             daemon['name'] = name
 
-        return Daemon(await self._session.post(f'daemons', data=daemon))
+        return Daemon(await self._session.post('daemons', data=daemon))
 
     async def get(self, daemon_id: str) -> Daemon:
         return Daemon(await self._session.get(f'daemons/{daemon_id}'))
+
+    async def update(self, daemon_id: str, *, name: Optional[str] = None, user: Optional[Union[str, User]] = None) -> Daemon:
+        data = {}
+
+        if name is not None:
+            data['name'] = name
+
+        if user is not None:
+            data['user'] = user.id if isinstance(user, User) else user
+
+        return Daemon(await self._session.put(f'daemons/{daemon_id}', data=data))
 
     async def delete(self, daemon_id: str) -> Daemon:
         return Daemon(await self._session.delete(f'daemons/{daemon_id}'))
--- lucifer/users/client.py
+++ lucifer/users/client.py
@@ -1,13 +1,14 @@
-from typing import List
+from typing import List, Optional
 
 from lucifer.bases.client import BaseClient
 from lucifer.users.user import SimpleUser, User
 
 
+# Client
 class UsersClient(BaseClient):
     # Methods
     async def list(self) -> List[SimpleUser]:
-        res = await self._session.get(f'users')
+        res = await self._session.get('users')
         return [SimpleUser(r) for r in res]
 
     async def create(self, email: str, password: str) -> User:
@@ -16,10 +17,21 @@ class UsersClient(BaseClient):
             'password': password
         }
 
-        return User(await self._session.post(f'users', data=user))
+        return User(await self._session.post('users', data=user))
 
     async def get(self, user_id: str) -> User:
         return User(await self._session.get(f'users/{user_id}'))
+
+    async def update(self, user_id: str, *, email: Optional[str] = None, password: Optional[str] = None) -> User:
+        data = {}
+
+        if email is not None:
+            data['email'] = email
+
+        if password is not None:
+            data['password'] = password
+
+        return User(await self._session.put(f'users/{user_id}', data=data))
 
     async def delete(self, user_id: str) -> User:
         return User(await self._session.delete(f'users/{user_id}'))
